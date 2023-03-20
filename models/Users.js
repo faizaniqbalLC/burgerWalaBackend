04997273f286b9651545838b26429149b4da4963
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-20 22:36:43 +0500

    work on models module

---
+++ models/Users.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema({
+  name: String,
+  photo: String,
+  googleId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Users = mongoose.model("User", schema);
