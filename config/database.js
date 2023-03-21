9f3b79336063329db3aa1e51bb4da5642c08ad9f
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-21 17:40:43 +0500

    added log message

--- config/database.js
+++ config/database.js
@@ -3,4 +3,5 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   const { connection } = await mongoose.connect(process.env.MONGO_URL);
   console.log("db conncted");
+  console.log("con");
 };
