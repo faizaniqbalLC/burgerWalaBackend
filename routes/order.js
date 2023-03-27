0416cb9d0747242ecd5f15f54b3ef010303f1d1c
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-27 22:36:20 +0500

    added admin order routes

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,20 @@
 import express from "express";
-import { placeOrder } from "../controllers/order.js";
-import { isAuthenticated } from "../middleware/auth.js";
+import {
+  getAdminOrders,
+  getMyOrders,
+  getOrderDetails,
+  placeOrder,
+  processOrder,
+} from "../controllers/order.js";
+import { authorizeAdmin, isAuthenticated } from "../middleware/auth.js";
+
 const router = express.Router();
 
 router.post("/createorder", placeOrder);
+router.get("/myorders", isAuthenticated, getMyOrders);
+router.get("/order/:id", isAuthenticated, getOrderDetails);
+
+// only admin can access by middleware
+router.get("/admin/orders", isAuthenticated, authorizeAdmin, getAdminOrders);
+router.get("/admin/order/:id", isAuthenticated, authorizeAdmin, processOrder);
 export default router;
