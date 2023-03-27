9fc47436421952b60a93b0a7ae2bc2421ea6eb83
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-27 22:39:55 +0500

    worked here

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 import express, { urlencoded } from "express";
 import dotenv from "dotenv";
+import { connectPassport } from "./utils/Provider.js";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import passport from "passport";
-
-import { connectPassport } from "./utils/Provider.js";
 import { errorMiddleware } from "./middleware/errorMiddleware.js";
-import userRouter from "./routes/user.js";
-import orderRouter from "./routes/order.js";
+import cors from "cors";
 
 const app = express();
-
 export default app;
-
 dotenv.config({
   path: "./config/config.env",
 });
+
+// Using Middlewares
+app.use(cors());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -33,10 +32,16 @@ app.use(
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
 app.use(passport.session());
+app.enable("trust proxy");
 
 connectPassport();
 
-// routes 1
-app.use("/api/v1", userRouter);
-app.use("/api/v1", orderRouter);
+// Importing Routes
+import userRoute from "./routes/user.js";
+import orderRoute from "./routes/order.js";
+
+app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
+
+// Using Error Middleware
 app.use(errorMiddleware);
