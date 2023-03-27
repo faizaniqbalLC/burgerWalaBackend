e645c34e400d0150270d3b81378ae31902c174ac
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-27 22:39:37 +0500

    added new order controllers

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,6 @@
 import { asyncError } from "../middleware/errorMiddleware.js";
 import { Order } from "../models/Order.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
 
 export const placeOrder = asyncError(async (req, res, next) => {
   const {
@@ -11,9 +12,10 @@ export const placeOrder = asyncError(async (req, res, next) => {
     shippingCharges,
     totalAmount,
   } = req.body;
-  const user = "req.user._id";
+
+  const user = req.user._id;
+
   const orderOptions = {
-    user,
     shippingInfo,
     orderItems,
     paymentMethod,
@@ -21,10 +23,64 @@ export const placeOrder = asyncError(async (req, res, next) => {
     taxPrice,
     shippingCharges,
     totalAmount,
+    user,
   };
+
   await Order.create(orderOptions);
+
+  res.status(201).json({
+    success: true,
+    message: "Order Placed Successfully via Cash On Delivery",
+  });
+});
+
+export const getMyOrders = asyncError(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id }).populate(
+    "user",
+    "name "
+  );
+
   res.status(200).json({
     success: true,
-    message: "Order placed successfully via Cash on Delivery",
+    orders,
+  });
+});
+
+export const getOrderDetails = asyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate("user", "name");
+
+  if (!order) {
+    return next(new ErrorHandler("Invalid Order Id", 404));
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+export const getAdminOrders = asyncError(async (req, res, next) => {
+  const orders = await Order.find({}).populate("user", "name ");
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+export const processOrder = asyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler("Invalid Order Id", 404));
+  }
+  if (order.orderStatus == "Preparing") order.orderStatus = "Shipped";
+  if (order.orderStatus == "Shipped") {
+    order.orderStatus = "Delivered";
+    order.deliveredAt = new Date(Date.now());
+  } else if (order.orderStatus == "Delivered") {
+    return next(new ErrorHandler("Food Already Delivered", 400));
+  }
+  await order.save();
+  res.status(200).json({
+    success: true,
+    message: "Status Updated Successfully!",
   });
 });
