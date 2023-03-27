f21a7929db242debe5cbff039f54b43c1cf0341a
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-27 22:38:04 +0500

    added new authorizeadmin middleware

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,6 +1,7 @@
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
