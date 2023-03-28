69cf79712fc63f154a03005a1d7b8805a9664c45
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-28 23:30:20 +0500

    worked on payments

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-import app from "./app.js"; 
+import app from "./app.js";
+import Razorpay from "razorpay";
 
 import { connectDb } from "./config/database.js";
 connectDb();
+
+export const instance = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
 
 app.get("/", (req, res, next) => {
   res.send("<h1>working fine</h1>");
