15fc8346184d26e33c3d9e7e1e3521871692ef15
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-29 12:47:30 +0500

    added origins

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ dotenv.config({
 });
 
 // Using Middlewares
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(
   session({
