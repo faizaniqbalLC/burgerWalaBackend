14417b5477b71e867fd4626c67855c3f2fd79803
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-29 12:30:21 +0500

    added admin routes

--- routes/user.js
+++ routes/user.js
@@ -10,13 +10,15 @@ router.get(
     scope: ["profile"],
   })
 );
-router.get("/login", passport.authenticate("google"), (req, res, next) => {
-  res.send("LOGGED IN");
-});
+router.get(
+  "/login",
+  passport.authenticate("google", { successRedirect: process.env.FRONTEND_URL })
+);
 
 router.get("/me", isAuthenticated, myProfile);
 router.get("/logout", logout);
 
 // admin routes
 router.get("/admin/user", isAuthenticated, authorizeAdmin, getAdminUsers);
+router.get("/admin/stats", isAuthenticated, authorizeAdmin);
 export default router;
