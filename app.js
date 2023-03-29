b8340f7b131f64a48ee30a92c8927a3b9c6bc3a6
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-29 12:31:20 +0500

    worked on app js

--- app.js
+++ app.js
@@ -14,12 +14,17 @@ dotenv.config({
 });
 
 // Using Middlewares
-app.use(cors());
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV == "development" ? false : true,
+      httpOnly: process.env.NODE_ENV == "development" ? false : true,
+      sameSite: process.env.NODE_ENV == "development" ? false : "none",
+    },
   })
 );
 app.use(cookieParser());
@@ -27,6 +32,13 @@ app.use(express.json());
 app.use(
   urlencoded({
     extended: true,
+  })
+);
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(passport.authenticate("session"));
