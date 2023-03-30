0812ee80bd546f6aed479513bd24c9a3368fdf4f
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-30 23:23:58 +0500

    origin URL changed

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://burger-wala-frontend-six.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
